74a83740e1139164a710489a8c609f795d9d105d
nathan95oks <nathanaelalba75@gmail.com>
2024-08-18 00:30:46 -0400

    version hola nombre genero edad espaniol ingles - bot

--- src/presenter.js
+++ src/presenter.js
@@ -6,18 +6,20 @@ const div = document.querySelector("#resultado-div");
 const nombreInput = document.querySelector("#nombre");
 const generoSelect = document.querySelector("#genero");
 const edadInput = document.querySelector("#edad");
+const idiomaSelect = document.querySelector("#idioma");
 
 // Añade un manejador de eventos para el formulario
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // Obtiene el nombre, género y edad ingresados
+  // Obtiene el nombre, género, edad e idioma ingresados
   const nombre = nombreInput.value;
   const genero = generoSelect.value;
   const edad = Number(edadInput.value);
+  const idioma = idiomaSelect.value;
 
-  // Llama a la función de saludo con el nombre, género y edad
-  const saludo = saludar(nombre, genero, edad);
+  // Llama a la función de saludo con el nombre, género, edad e idioma
+  const saludo = saludar(nombre, genero, edad, idioma);
 
   // Muestra el saludo en el div
   div.innerHTML = `<p>${saludo}</p>`;
--- src/saludador.js
+++ src/saludador.js
@@ -1,20 +1,42 @@
-function saludar(nombre, genero, edad) {
-  let saludo = `Hola, ${nombre}`;
+function saludar(nombre, genero, edad, idioma) {
+  let saludo;
 
-  if (genero === "masculino") {
-    saludo += ", señor";
-  } else if (genero === "femenino") {
-    saludo += ", señora";
-  } else {
-    saludo += ", amig@";
-  }
+  if (idioma === "espanol") {
+    saludo = `Hola, ${nombre}`;
 
-  if (edad < 18) {
-    saludo += ". ¡Eres joven!";
-  } else if (edad < 60) {
-    saludo += ". ¡Qué gusto saludarte!";
-  } else {
-    saludo += ". ¡Un placer conocerte!";
+    if (genero === "masculino") {
+      saludo += ", señor";
+    } else if (genero === "femenino") {
+      saludo += ", señora";
+    } else {
+      saludo += ", amig@";
+    }
+
+    if (edad < 18) {
+      saludo += ". ¡Eres joven!";
+    } else if (edad < 60) {
+      saludo += ". ¡Qué gusto saludarte!";
+    } else {
+      saludo += ". ¡Un placer conocerte!";
+    }
+  } else if (idioma === "ingles") {
+    saludo = `Hello, ${nombre}`;
+
+    if (genero === "masculino") {
+      saludo += ", sir";
+    } else if (genero === "femenino") {
+      saludo += ", madam";
+    } else {
+      saludo += ", friend";
+    }
+
+    if (edad < 18) {
+      saludo += ". You're young!";
+    } else if (edad < 60) {
+      saludo += ". Nice to meet you!";
+    } else {
+      saludo += ". A pleasure to meet you!";
+    }
   }
 
   return saludo;
